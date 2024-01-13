0832cfaf3fbd2c6268211a33bf4778b6a0464b77
coolwhipki <kirajanegirl@gmail.com>
2024-01-13 11:11:59 -0700

    For Mo

---
+++ Wordle.py
@@ -0,0 +1,43 @@
+# File: Wordle.py
+
+"""
+This module is the starter file for the Wordle assignment.
+BE SURE TO UPDATE THIS COMMENT WHEN YOU WRITE THE CODE.
+"""
+
+import random
+
+from WordleDictionary import FIVE_LETTER_WORDS
+from WordleGraphics import WordleGWindow, N_COLS, N_ROWS
+
+def wordle():
+
+    def enter_action(s):
+        # Array that stores the letters in the squares before these letters are combined into a word
+        guess = []
+
+        #check to see if the word the user entered is a real word (in the word list)
+        for guessLetter in range(N_COLS):
+            guess.append(gw.get_square_letter(0, guessLetter))
+        
+        print(guess)
+
+        wordList = FIVE_LETTER_WORDS
+
+        # pick a word to guess from the word list provided in the WordleDictionary.py
+        correctWord = random.choice(wordList)
+
+        #Go through letter in the word 
+        for letter in range(N_COLS):
+            gw.set_square_letter(N_ROWS-1, letter, correctWord[letter])
+
+        gw.show_message("You have to implement this method.")
+
+    gw = WordleGWindow()
+    gw.add_enter_listener(enter_action)
+
+
+# Startup code
+
+if __name__ == "__main__":
+    wordle()
