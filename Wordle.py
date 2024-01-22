70ec565d81a449b0ade913bb14ad6d8df5b2d15b
coolwhipki <kirajanegirl@gmail.com>
2024-01-22 13:41:33 -0700

    Correct word is constant and compare users guess

--- Wordle.py
+++ Wordle.py
@@ -11,8 +11,14 @@ from WordleDictionary import FIVE_LETTER_WORDS
 from WordleGraphics import WordleGWindow, N_COLS, N_ROWS
 
 def wordle():
+    # initialized English word list dictionary
+    wordList = FIVE_LETTER_WORDS
+
+    # pick a word to guess from the word list provided in the WordleDictionary.py
+    correctWord = random.choice(wordList).lower()
 
     def enter_action(s):
+
         #function that gets the word from the squares and makes it a word
         def createWord():
             # Array that stores the letters in the squares before these letters are combined into a word
@@ -27,24 +33,19 @@ def wordle():
         
         
 
-        # initialized English word list dictionary
-        wordList = FIVE_LETTER_WORDS
-
-        # pick a word to guess from the word list provided in the WordleDictionary.py
-        correctWord = random.choice(wordList).upper()
-
         #Calls createWord function to pull user input guess
         guessWord = createWord()
 
+
         #Go through letter in the word 
         for letter in range(N_COLS):
-            gw.set_square_letter(N_ROWS-1, letter, correctWord[letter])
+            gw.set_square_letter(N_ROWS-1, letter, correctWord[letter].upper())
 
         # Check to see if
-        if guessWord in wordList:
-            gw.show_message("Slay thats a word")
+        if guessWord == correctWord:
+            gw.show_message("Slay that is the word")
         else:
-            gw.show_message("Not in word list")
+            gw.show_message("Guess again muchacho")
 
 
 
