e49a963778aaf78ebdb4aadefc641b1930f59c7a
coolwhipki <kirajanegirl@gmail.com>
2024-01-23 14:09:05 -0700

    color the squares and added colorblind alts

--- Wordle.py
+++ Wordle.py
@@ -8,7 +8,7 @@ BE SURE TO UPDATE THIS COMMENT WHEN YOU WRITE THE CODE.
 import random
 
 from WordleDictionary import FIVE_LETTER_WORDS
-from WordleGraphics import WordleGWindow, N_COLS, N_ROWS
+from WordleGraphics import WordleGWindow, N_COLS, N_ROWS, CORRECT_COLOR, PRESENT_COLOR, MISSING_COLOR
 
 def wordle():
     # initialized English word list dictionary
@@ -25,7 +25,7 @@ def wordle():
             guessArray = []
             #check to see if the word the user entered is a real word (in the word list)
             for guessLetter in range(N_COLS):
-                guessArray.append(gw.get_square_letter(0, guessLetter))
+                guessArray.append(gw.get_square_letter(gw.get_current_row(), guessLetter))
         
             #combine the array into a word
             word = ''.join(guessArray).lower()
@@ -36,17 +36,65 @@ def wordle():
         #Calls createWord function to pull user input guess
         guessWord = createWord()
 
+        if guessWord in wordList:
+            #Go through letter in the word print to last row
+            for letter in range(N_COLS):
+                gw.set_square_letter(N_ROWS-1, letter, correctWord[letter].upper())
 
-        #Go through letter in the word 
-        for letter in range(N_COLS):
-            gw.set_square_letter(N_ROWS-1, letter, correctWord[letter].upper())
 
-        # Check to see if
-        if guessWord == correctWord:
-            gw.show_message("Slay that is the word")
+
+            # Color the squares based on how close their guess is to the correct word
+            # Lists of booleans to keep track of correct and present letters
+            correct_letters_matched = [False] * N_COLS
+            present_letters_matched = [False] * N_COLS
+
+            # If the user has checked the color blind option it changes the colors for boxes
+            colorBlind = True
+
+            #if color blind is true then change the colors that are passed to set_square_color to colorblind safe colors
+            if colorBlind == True:
+                correct = "#000080"
+                present = "#DAA520"
+                missing = "#D3D3D3"
+            else:
+                correct = CORRECT_COLOR
+                present = PRESENT_COLOR
+                missing = MISSING_COLOR
+
+            # Check if the guess is correct
+            # Color the boxes based on correct and present letters
+            for square in range(N_COLS):
+                # Make words lowercase for correct comparison and iterate through each letter of each word
+                guess_letter = guessWord[square].lower()
+                correct_letter = correctWord[square].lower()
+
+                # Checks if the guess letter is the correct letter and also checks if that letter in that current position has been 
+                #found in the correct position
+                if guess_letter == correct_letter and not correct_letters_matched[square]:
+                    gw.set_square_color(gw.get_current_row(), square, correct)
+                    correct_letters_matched[square] = True
+
+                # Checks if guess letter is in the correct word and checks if letter in current position has already been 
+                #found in the correctword
+                elif guess_letter in correctWord and not present_letters_matched[square]:
+                    gw.set_square_color(gw.get_current_row(), square, present)
+                    present_letters_matched[square] = True
+
+                # Not in correct word
+                else:
+                    gw.set_square_color(gw.get_current_row(), square, missing)
+
+            # If all the squares are green that means it is the correct word so put a congratulatory message
+            if all(correct_letters_matched):
+                gw.show_message("Congratulations! That is the word.")
+            # Didn't guess the right word so moves you down a row and tells you to guess again
+            else:
+                gw.set_current_row(gw.get_current_row() + 1)
+                gw.show_message("Guess again, muchacho")
+
+        # If the word is not in the word list (not an english word) does not run all the checks
         else:
-            gw.show_message("Guess again muchacho")
-
+            gw.show_message("Not in word list")
 
 
 
