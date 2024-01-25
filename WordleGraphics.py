40ef3b5d5ddb8b351ef51f36f10268ee6094c508
Marcus Mosiah Galbraith <marcusg4byu@gmail.com>
2024-01-25 10:26:41 -0700

    mo backspace lol

--- WordleGraphics.py
+++ WordleGraphics.py
@@ -110,7 +110,7 @@ class WordleGWindow:
                 self.show_message("")
                 s = ""
                 for col in range(N_COLS):
-                    s += self._grid[self._row][col].get_letter();
+                    s += self._grid[self._row][col].get_letter()
                 for fn in self._enter_listeners:
                     fn(s)
             elif ch.isalpha():
@@ -119,6 +119,13 @@ class WordleGWindow:
                     sq = self._grid[self._row][self._col]
                     sq.set_letter(ch)
                     self._col += 1
+            elif ch == "BACKSPACE":
+                self.show_message("")
+                if self._row < N_ROWS and self._col > 0:
+                    self._col -= 1
+                    sq = self._grid[self._row][self._col]
+                    sq.set_letter(" ")
+
 
         def press_action(tke):
             self._down_x = tke.x
